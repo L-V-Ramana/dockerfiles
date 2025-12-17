537d8df88141e192fb8a43b95c3599296a46fd6c
L V Ramana <lellalvramana@gmail.com>
2025-12-17 13:28:33 +0530

    docker practice

---
+++ DockerInstance/docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+systemctl enable docker 
+systemctl start docker 
+
+usermod -aG docker ec2-user
